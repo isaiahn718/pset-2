49d915371df288146c284f9be3b5cff06c6cf95a
Isaiah Nunez <INunez@ucvts.tec.nj.us>
2019-10-23 13:15:48 -0400

    commit 2

---
+++ src/area.js
@@ -0,0 +1,5 @@
+const readlineSync = require('readline-sync')
+let length = readlineSync.question("\nLength:\n")
+let width = readlineSync.question("\nWidth:\n")
+const milimeters = 25.4
+let  = Width_in_inch * Length_in_inch = 
