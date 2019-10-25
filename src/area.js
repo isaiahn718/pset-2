b0585dfa12af52b47f043fa9b74aeed22be511a1
Isaiah Nunez <INunez@ucvts.tec.nj.us>
2019-10-25 12:35:51 -0400

    fourth

--- src/area.js
+++ src/area.js
@@ -1,5 +1,11 @@
-const readlineSync = require('readline-sync')
-let length = readlineSync.question("\nLength:\n")
-let width = readlineSync.question("\nWidth:\n")
-const milimeters = 25.4
-let  = Width_in_inch * Length_in_inch = 
+const readlineSync = require("readline-sync");
+
+const Width = readlineSync.question("Width: ");
+const Length = readlineSync.question("Length: ");
+let w= Width;
+let l= Length;
+let d= Math.sqrt((w*w + l*l))
+
+d= d.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+console.log("\nA(n) " + w + "-by-" + l + "-inch sheet of paper has an diagonal of " + d + " square inch(s).");
