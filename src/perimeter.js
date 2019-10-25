99a08dacddfdaa1221da5d202fbf4bd634acf983
Isaiah Nunez <INunez@ucvts.tec.nj.us>
2019-10-25 12:37:03 -0400

    fifth

--- src/perimeter.js
+++ src/perimeter.js
@@ -2,13 +2,13 @@ const readlineSync = require("readline-sync");
 
 const Width = readlineSync.question("Width: ");
 const Length = readlineSync.question("Length: ");
-const Conversion = 25.4;
+const Conversion = 2.54;
 let w= Width;
 let l=Length;
 let k=w*Conversion;
 let s=l*Conversion;
-let f= k*s;
+let f=2*k + 2*s;
 let r=Math.round (f)
 f= f.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
-console.log("\nA(n) " + w + "-by-" + l + "-inch sheet of paper has an area of " + f + " square millimeter(s).");
+console.log("\nA(n) " + w + "-by-" + l + "-inch sheet of paper has a perimeter of " + f + " centimeters(s).");
